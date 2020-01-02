d14f9819bc23c306ed322d934cbb8caf7a4a1d04
aagrosse <aagrock81@yahoo.com>
2020-01-02 14:43:07 -0500

    fixed high scores issues

--- assets/script.js
+++ assets/script.js
@@ -7,26 +7,26 @@ var questions = [
       choices: ["strings", "booleans", "alerts", "numbers"],
       answer: "alerts"
     },
-    // {
-    //   title: "The condition in an if / else statement is enclosed within ____.",
-    //   choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    //   answer: "parentheses"
-    // },
-    // {
-    //   title: "Arrays in JavaScript can be used to store _________",
-    //   choices: ["numbers and strings", "other arrays", "booleans", "all-of-the-above"],
-    //   answer: "all-of-the-above"
-    // },
-    // {
-    //   title: "String values must be enclosed within _____ when being assigned to variables.",
-    //   choices: ["commas", "curly brackets", "quotes", "parentheses"],
-    //   answer: "quotes"
-    // },
-    // {
-    //   title: "A very useful tool used during debuggingfor printing content to the debugger is:",
-    //   choices: ["JavaScript", "terminal/ bash", "for loops", "console.log"],
-    //   answer: "console.log"
-    //   },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store _________",
+      choices: ["numbers and strings", "other arrays", "booleans", "all-of-the-above"],
+      answer: "all-of-the-above"
+    },
+    {
+      title: "String values must be enclosed within _____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      title: "A very useful tool used during debuggingfor printing content to the debugger is:",
+      choices: ["JavaScript", "terminal/ bash", "for loops", "console.log"],
+      answer: "console.log"
+      },
 ]
 var secondsLeft = (questions.length) * 15;
 var highScoreList = document.querySelector("#high-score");
@@ -45,6 +45,7 @@ $("#highScores").on("click", function () {
     console.log("hello World");
     $('.show-onclick').empty();
     $('.empty').empty();
+    init()
     scores();
 });
 
